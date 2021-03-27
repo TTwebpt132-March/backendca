151867b4bd501dc04793fcf480cc10419a3b4be0
Carlos Ramos <rmscrlos@gmail.com>
2021-03-27 13:39:57 -0400

    made some changes

--- api/middlewares/auth-middeware.js
+++ api/middlewares/auth-middeware.js
@@ -36,29 +36,33 @@ const checkBodyRegister = (req, res, next) => {
 };
 
 const checkBodyLogin = async (req, res, next) => {
-	const { username: user_username, password: user_password } = req.body;
+	try {
+		const { username, password } = req.body;
 
-	if (!user_username || !user_password) {
-		return res.status(401).json({
-			message: 'Username and password required.'
-		});
+		const user_username = username;
+		if (!username || !password) {
+			return res.status(401).json({
+				message: 'Username and password required.'
+			});
+		}
+
+		const user = await Users.findBy({ user_username }).first();
+		if (!user) {
+			return res.status(401).json({
+				message: 'Invalid Credentials.'
+			});
+		}
+		const passwordValid = await bcrypt.compare(password, user.user_password);
+
+		if (!passwordValid) {
+			return res.status(401).json({
+				message: 'Invalid Credentials.'
+			});
+		}
+		next();
+	} catch (err) {
+		next(err);
 	}
-
-	const user = await Users.findBy({ user_username }).first();
-	if (!user) {
-		return res.status(401).json({
-			message: 'Invalid Credentials.'
-		});
-	}
-	const passwordValid = await bcrypt.compare(user_password, user.user_password);
-
-	if (!passwordValid) {
-		return res.status(401).json({
-			message: 'Invalid Credentials.'
-		});
-	}
-
-	next();
 };
 
 const checkUsernameExists = async (req, res, next) => {
