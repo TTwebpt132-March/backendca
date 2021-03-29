4bf760add4a2e083df7bd59a4b4dd93997842fb9
Carlos Ramos <rmscrlos@gmail.com>
2021-03-29 18:39:00 -0400

    heroku push

--- api/middlewares/restricted.js
+++ api/middlewares/restricted.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
 	try {
 		const token = req.cookies.token || req.header.Authorization;
 
-		console.log(token);
+		console.log(req.headers);
 		if (!token) {
 			return res.status(401).json({
 				message: 'Token required.'
